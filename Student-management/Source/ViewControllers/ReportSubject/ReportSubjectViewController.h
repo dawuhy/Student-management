f4ec911ee87fe93b75d7c79c0efcc68e25c8841d
HuyDQ <huydq@runsystem.net>
2021-01-18 08:34:20 +0700

    Init layout ReportSubject

---
+++ Student-management/Source/ViewControllers/ReportSubject/ReportSubjectViewController.h
@@ -0,0 +1,17 @@
+//
+//  ReportSubjectViewController.h
+//  Student-management
+//
+//  Created by Dang Quoc Huy on 1/17/21.
+//  Copyright © 2021 Dang Quoc Huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ReportSubjectViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

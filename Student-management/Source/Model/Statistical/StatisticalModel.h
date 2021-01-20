e57d58648afb9568c564c2daca58a4834c170f6d
HuyDQ <huydq@runsystem.net>
2021-01-20 21:50:30 +0700

    Add feature report statistical, complete app

---
+++ Student-management/Source/Model/Statistical/StatisticalModel.h
@@ -0,0 +1,17 @@
+//
+//  StatisticalModel.h
+//  Student-management
+//
+//  Created by Dang Quoc Huy on 1/20/21.
+//  Copyright © 2021 Dang Quoc Huy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StatisticalModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Student-management/Source/Model/Statistical/StatisticalModel.m
@@ -0,0 +1,13 @@
+//
+//  StatisticalModel.m
+//  Student-management
+//
+//  Created by Dang Quoc Huy on 1/20/21.
+//  Copyright © 2021 Dang Quoc Huy. All rights reserved.
+//
+
+#import "StatisticalModel.h"
+
+@implementation StatisticalModel
+
+@end
